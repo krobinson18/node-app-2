e438bcdaec8d5eb80af5cca6149901b73872d58d
Kisha Robinson <krobinson@Kishas-iMac.local>
2018-11-09 09:43:31 -0500

    Modify response header message.

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ var request = require('supertest');
 var app = require('../index.js');
 
 describe('GET /', function() {
-  it('respond with hello world', function(done) {
+  it('respond with hey world', function(done) {
 
     // navigate to the root and check the response is "hello world"
-    request(app).get('/').expect('hey world i\'m doing this thing!', done);
+    request(app).get('/').expect('hey world this is a test configuration!', done);
   });
 });
