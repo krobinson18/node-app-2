db49b2c0846f9a18c602e7ee2123e59e953e3970
Kisha Robinson <krobinson@Kishas-iMac.local>
2018-11-09 11:45:17 -0500

    Revert stuff to make things pretty again.

--- test/test.js
+++ test/test.js
@@ -2,9 +2,13 @@ var request = require('supertest');
 var app = require('../index.js');
 
 describe('GET /', function() {
-  it('respond with hey world', function(done) {
+  it('respond with hello world', function(done) {
 
     // navigate to the root and check the response is "hello world"
+<<<<<<< HEAD
     request(app).get('/').expect('hey world this is a test configuration after dependencies have been fixed!', done);
+=======
+    request(app).get('/').expect('hey world i\'m doing this thing!', done);
+>>>>>>> parent of e438bcd... Modify response header message.
   });
 });
