8b01b4ec6f217e04908a86419a8eaccaf0fefed4
Kisha Robinson - QA <44809932+krobinson18@users.noreply.github.com>
2018-11-09 12:03:57 -0500

    update test.js

--- test/test.js
+++ test/test.js
@@ -5,10 +5,7 @@ describe('GET /', function() {
   it('respond with hello world', function(done) {
 
     // navigate to the root and check the response is "hello world"
-<<<<<<< HEAD
     request(app).get('/').expect('hey world this is a test configuration after dependencies have been fixed!', done);
-=======
     request(app).get('/').expect('hey world i\'m doing this thing!', done);
->>>>>>> parent of e438bcd... Modify response header message.
   });
 });
