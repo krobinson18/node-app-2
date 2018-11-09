006e99269785ea906c8f368fb019844498274dbd
Kisha Robinson - QA <44809932+krobinson18@users.noreply.github.com>
2018-11-09 12:05:24 -0500

    index.js

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-<<<<<<< HEAD
- res.send('hey world this is a test configuration after dependencies have been fixed!');
-=======
  res.send('hey world i\'m doing this thing!');
->>>>>>> parent of e438bcd... Modify response header message.
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
